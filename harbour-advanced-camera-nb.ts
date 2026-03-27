1c0bccef82f3b9656757d0d6cf640a2ef15be8d0
Frank Paul Silye <frankps@gmail.com>
2026-03-27 16:23:00 +0100

    Add files via upload

---
+++ harbour-advanced-camera-nb.ts
@@ -0,0 +1,361 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO"">
+<context>
+    <name>CameraUI</name>
+    <message>
+        <source>Camera: </source>
+        <translation>Kamera: </translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Advanced Camera</source>
+        <translation>Avansert kamera</translation>
+    </message>
+</context>
+<context>
+    <name>EffectsModel</name>
+    <message>
+        <source>None</source>
+        <translation>Ingen</translation>
+    </message>
+    <message>
+        <source>Aqua</source>
+        <translation>Vann</translation>
+    </message>
+    <message>
+        <source>Blackboard</source>
+        <translation>Blackboard</translation>
+    </message>
+    <message>
+        <source>Grayscale</source>
+        <translation>Gråskala</translation>
+    </message>
+    <message>
+        <source>Negative</source>
+        <translation>Negativ</translation>
+    </message>
+    <message>
+        <source>Posterize</source>
+        <translation>Reduser antall farger</translation>
+    </message>
+    <message>
+        <source>Sepia</source>
+        <translation>Sepia</translation>
+    </message>
+    <message>
+        <source>Solarize</source>
+        <translation>Solariser</translation>
+    </message>
+    <message>
+        <source>Whiteboard</source>
+        <translation>Whiteboard</translation>
+    </message>
+    <message>
+        <source>Emboss</source>
+        <translation>Relieff</translation>
+    </message>
+    <message>
+        <source>Sketch</source>
+        <translation>Skiss</translation>
+    </message>
+    <message>
+        <source>Neon</source>
+        <translation>Neon</translation>
+    </message>
+</context>
+<context>
+    <name>ExposureModel</name>
+    <message>
+        <source>Automatic Scene Detection</source>
+        <translation>Automatisk scenegjenkjenning</translation>
+    </message>
+    <message>
+        <source>Augmented Reality</source>
+        <translation>Utvidet virkelighet</translation>
+    </message>
+    <message>
+        <source>Backlight</source>
+        <translation>Motlys</translation>
+    </message>
+    <message>
+        <source>Barcode</source>
+        <translation>Strekkode</translation>
+    </message>
+    <message>
+        <source>Beach</source>
+        <translation>Strand</translation>
+    </message>
+    <message>
+        <source>Candlelight</source>
+        <translation>Stearinlys</translation>
+    </message>
+    <message>
+        <source>Fireworks</source>
+        <translation>Fyrverkeri</translation>
+    </message>
+    <message>
+        <source>Flowers</source>
+        <translation>Blomster</translation>
+    </message>
+    <message>
+        <source>HDR</source>
+        <translation>HDR</translation>
+    </message>
+    <message>
+        <source>Landscape</source>
+        <translation>Landskap</translation>
+    </message>
+    <message>
+        <source>Large Aperture</source>
+        <translation>Stor blenderåpning</translation>
+    </message>
+    <message>
+        <source>Night</source>
+        <translation>Natt</translation>
+    </message>
+    <message>
+        <source>Night Portrait</source>
+        <translation>Nattportrett</translation>
+    </message>
+    <message>
+        <source>Party</source>
+        <translation>Party</translation>
+    </message>
+    <message>
+        <source>Portrait</source>
+        <translation>Portrett</translation>
+    </message>
+    <message>
+        <source>Small Aperture</source>
+        <translation>Liten blenderåpning</translation>
+    </message>
+    <message>
+        <source>Snow</source>
+        <translation>Snø</translation>
+    </message>
+    <message>
+        <source>Sports</source>
+        <translation>Sport</translation>
+    </message>
+    <message>
+        <source>Spotlight</source>
+        <translation>Spotlight</translation>
+    </message>
+    <message>
+        <source>Steady Photo</source>
+        <translation>Stabilt foto</translation>
+    </message>
+    <message>
+        <source>Sunset</source>
+        <translation>Solnedgang</translation>
+    </message>
+    <message>
+        <source>Theatre</source>
+        <translation>Teater</translation>
+    </message>
+    <message>
+        <source>Action</source>
+        <translation>Action</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Av</translation>
+    </message>
+</context>
+<context>
+    <name>FlashModel</name>
+    <message>
+        <source>Unknown</source>
+        <translation>Ukjent</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Auto</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Av</translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation>På</translation>
+    </message>
+    <message>
+        <source>Red Eye Reduction</source>
+        <translation>Reduksjon av røde øyne</translation>
+    </message>
+    <message>
+        <source>Fill</source>
+        <translation>Fyll</translation>
+    </message>
+    <message>
+        <source>Torch</source>
+        <translation>Fakkel</translation>
+    </message>
+    <message>
+        <source>Video Light</source>
+        <translation>Videolys</translation>
+    </message>
+    <message>
+        <source>Slow Sync Front Curtain</source>
+        <translation>Langsynkronisering — forreste gardin</translation>
+    </message>
+    <message>
+        <source>Slow Sync Rear Curtain</source>
+        <translation>Langsynkronisering — bakerste gardin</translation>
+    </message>
+    <message>
+        <source>Manual</source>
+        <translation>Manuell</translation>
+    </message>
+</context>
+<context>
+    <name>FocusModel</name>
+    <message>
+        <source>Manual</source>
+        <translation>Manuell</translation>
+    </message>
+    <message>
+        <source>Hyperfocal</source>
+        <translation>Hyperfokal</translation>
+    </message>
+    <message>
+        <source>Infinity</source>
+        <translation>Uendelig</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Auto</translation>
+    </message>
+    <message>
+        <source>Continuous</source>
+        <translation>Kontinuerlig</translation>
+    </message>
+    <message>
+        <source>Macro</source>
+        <translation>Makro</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Ukjent</translation>
+    </message>
+</context>
+<context>
+    <name>GalleryUI</name>
+    <message>
+        <source>Deleting %1</source>
+        <translation>Sletter %1</translation>
+    </message>
+</context>
+<context>
+    <name>IsoModel</name>
+    <message>
+        <source>Auto ISO</source>
+        <translation>Auto-ISO</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsOverlay</name>
+    <message>
+        <source>Swap zoom controls</source>
+        <translation>Bytt om på zoomkontrollene</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Ingen</translation>
+    </message>
+    <message>
+        <source>Thirds</source>
+        <translation>Tredjedelsrutenett</translation>
+    </message>
+    <message>
+        <source>Ambience</source>
+        <translation>Ambiens</translation>
+    </message>
+    <message>
+        <source>Grid:</source>
+        <translation>Rutenett:</translation>
+    </message>
+    <message>
+        <source>Video Bitrate</source>
+        <translation>Videobitrate</translation>
+    </message>
+    <message>
+        <source>Audio Bitrate</source>
+        <translation>Lydbitrate</translation>
+    </message>
+    <message>
+        <source>Store GPS location to metadata</source>
+        <translation>Lagre GPS‑posisjon i metadata</translation>
+    </message>
+    <message>
+        <source>Enable wide camera selection</source>
+        <translation>Aktiver valg av vidvinkelkamera</translation>
+    </message>
+    <message>
+        <source>Disabled Cameras</source>
+        <translation>Deaktiverte kameraer</translation>
+    </message>
+    <message>
+        <source>Display manual controls</source>
+        <translation>Vis manuelle kontroller</translation>
+    </message>
+</context>
+<context>
+    <name>StorageModel</name>
+    <message>
+        <source>Internal storage</source>
+        <translation>Intern lagring</translation>
+    </message>
+</context>
+<context>
+    <name>WbModel</name>
+    <message>
+        <source>Auto</source>
+        <translation>Auto</translation>
+    </message>
+    <message>
+        <source>Manual</source>
+        <translation>Manuell</translation>
+    </message>
+    <message>
+        <source>Sunlight</source>
+        <translation>Sollys</translation>
+    </message>
+    <message>
+        <source>Cloudy</source>
+        <translation>Skyet</translation>
+    </message>
+    <message>
+        <source>Shade</source>
+        <translation>Skygge</translation>
+    </message>
+    <message>
+        <source>Tungsten</source>
+        <translation>Tungsten</translation>
+    </message>
+    <message>
+        <source>Flash</source>
+        <translation>Flash</translation>
+    </message>
+    <message>
+        <source>Sunset</source>
+        <translation>Solnedgang</translation>
+    </message>
+    <message>
+        <source>Warm Fluorescent</source>
+        <translation>Varmt fluorescerende</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Ukjent</translation>
+    </message>
+    <message>
+        <source>Fluorescent</source>
+        <translation>Fluorescerende</translation>
+    </message>
+</context>
+</TS>
