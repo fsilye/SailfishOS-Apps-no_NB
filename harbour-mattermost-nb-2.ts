0d04147c25e49eebe967553005efcddd1fd343a5
Frank Paul Silye <frankps@gmail.com>
2026-03-27 16:49:37 +0100

    Add files via upload

---
+++ harbour-mattermost-nb-2.ts
@@ -0,0 +1,521 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Version: </source>
+        <translation>Versjon: </translation>
+    </message>
+    <message>
+        <source>This is unofficial client for</source>
+        <translation>Dette er en uoffisiell klient for</translation>
+    </message>
+    <message>
+        <source>server.</source>
+        <translation>server.</translation>
+    </message>
+    <message>
+        <source>Thanks to</source>
+        <translation>Takk til</translation>
+    </message>
+    <message>
+        <source>Russian SailfishOS Community channel</source>
+        <translation>Russisk SailfishOS-fellesskapskanal</translation>
+    </message>
+    <message>
+        <source>Sources: </source>
+        <translation>Kildekode: </translation>
+    </message>
+    <message>
+        <source>in Telegram for their help.</source>
+        <translation>i Telegram for deres hjelp.</translation>
+    </message>
+    <message>
+        <source>Using Emoji from</source>
+        <translation>Bruk av emoji fra</translation>
+    </message>
+    <message>
+        <source>Emoji categories icon design - </source>
+        <translation>Emoji-kategorier ikondesign - </translation>
+    </message>
+    <message>
+        <source> from Junnxy studio (links below):</source>
+        <translation>fra Junnxy studio (lenker nedenfor):</translation>
+    </message>
+    <message>
+        <source>Thanks to users (contribute and testing):</source>
+        <translation>Takk til brukerne (bidrag og testing):</translation>
+    </message>
+    <message>
+        <source>If you want to donate, you can do that by:</source>
+        <translation>Hvis du ønsker å donere, kan du gjøre det ved å:</translation>
+    </message>
+</context>
+<context>
+    <name>AccountsPage</name>
+    <message>
+        <source>Accounts</source>
+        <translation>Kontoer</translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation>Tilkoblet</translation>
+    </message>
+    <message>
+        <source>Connecting</source>
+        <translation>Kobler til</translation>
+    </message>
+    <message>
+        <source>Offline</source>
+        <translation>Frakoblet</translation>
+    </message>
+    <message>
+        <source>name: </source>
+        <translation>navn: </translation>
+    </message>
+    <message>
+        <source>url: </source>
+        <translation>url: </translation>
+    </message>
+    <message>
+        <source>status: </source>
+        <translation>status: </translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Gi nytt navn</translation>
+    </message>
+    <message>
+        <source>Disable</source>
+        <translation>Deaktiver</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Fjern</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Valg</translation>
+    </message>
+    <message>
+        <source>Add account ...</source>
+        <translation>Legg til konto...</translation>
+    </message>
+    <message>
+        <source>Enable</source>
+        <translation>Aktiver</translation>
+    </message>
+    <message>
+        <source>Loggining</source>
+        <translation>Logging</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Deaktivert</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelLabel</name>
+    <message>
+        <source>Public channes</source>
+        <translation>Åpne kanaler</translation>
+    </message>
+    <message>
+        <source>Private channes</source>
+        <translation>Private kanaler</translation>
+    </message>
+    <message>
+        <source>Direct channes</source>
+        <translation>Direktemeldinger</translation>
+    </message>
+    <message>
+        <source>Typing</source>
+        <translation>Skriver</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelsPage</name>
+    <message>
+        <source>Options</source>
+        <translation>Valg</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Offline</source>
+        <translation>Avlogget</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Feil</translation>
+    </message>
+    <message>
+        <source>Online</source>
+        <translation>Pålogget</translation>
+    </message>
+    <message>
+        <source>Connecting</source>
+        <translation>Kobler til</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Login</source>
+        <translation>Logg på</translation>
+    </message>
+    <message>
+        <source>server custom name</source>
+        <translation>Egendefinert servernavn</translation>
+    </message>
+    <message>
+        <source>server address</source>
+        <translation>serveradresse</translation>
+    </message>
+    <message>
+        <source>use authentication token</source>
+        <translation>bruk autentiseringstoken</translation>
+    </message>
+    <message>
+        <source>Authentication token</source>
+        <translation>Autentiseringstoken</translation>
+    </message>
+    <message>
+        <source>Username or Email address</source>
+        <translation>Brukernavn eller e-postadresse</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Passord</translation>
+    </message>
+    <message>
+        <source>Certificate options</source>
+        <translation>Sertifikatalternativer</translation>
+    </message>
+    <message>
+        <source>trust certificate</source>
+        <translation>stol på sertifikatet</translation>
+    </message>
+    <message>
+        <source>Choose CA certificate</source>
+        <translation>Velg CA-sertifikat</translation>
+    </message>
+    <message>
+        <source>CA certificate path</source>
+        <translation>Sti til CA-sertifikat</translation>
+    </message>
+    <message>
+        <source>Choose server certificate</source>
+        <translation>Velg serversertifikat</translation>
+    </message>
+    <message>
+        <source>Server certificate path</source>
+        <translation>Sti til serversertifikat</translation>
+    </message>
+</context>
+<context>
+    <name>MattermostQt</name>
+    <message>
+        <source>Cant open CA certificate file: &quot;%0&quot;</source>
+        <translation>Kan ikke åpne CA-sertifikatfilen: &quot;%0&quot;</translation>
+    </message>
+    <message>
+        <source>Cant open certificate file: &quot;%0&quot;</source>
+        <translation>Kan ikke åpne sertifikatfilen: &quot;%0&quot;</translation>
+    </message>
+    <message>
+        <source>File: id(%0); </source>
+        <translation>Fil: id(%0); </translation>
+    </message>
+    <message>
+        <source>Image: </source>
+        <translation>Bilde: </translation>
+    </message>
+    <message>
+        <source>File: </source>
+        <translation>Fil: </translation>
+    </message>
+    <message>
+        <source>Empty message</source>
+        <translation>Tom melding</translation>
+    </message>
+</context>
+<context>
+    <name>MessageEditorBar</name>
+    <message>
+        <source>Message...</source>
+        <translation>Melding...</translation>
+    </message>
+    <message>
+        <source>Uploading </source>
+        <translation>Laster opp </translation>
+    </message>
+    <message>
+        <source>Choose image</source>
+        <translation>Velg bilde</translation>
+    </message>
+    <message>
+        <source>Choose document</source>
+        <translation>Velg dokument</translation>
+    </message>
+    <message>
+        <source>Choose file</source>
+        <translation>Velg fil</translation>
+    </message>
+</context>
+<context>
+    <name>MessagesPage</name>
+    <message>
+        <source>get older</source>
+        <translation>Vis eldre meldinger</translation>
+    </message>
+    <message>
+        <source>Reply</source>
+        <translation>Svar</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Rediger</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopier</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Slett</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Sletter</translation>
+    </message>
+    <message>
+        <source>Add reaction</source>
+        <translation>Legg til reaksjon</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsPage</name>
+    <message>
+        <source>Options</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <source>Show blobs</source>
+        <translation>Vis bobler</translation>
+    </message>
+    <message>
+        <source>Show blobs unders messages</source>
+        <translation>Vis bobler under meldinger</translation>
+    </message>
+    <message>
+        <source>Blobs opacity value</source>
+        <translation>Gjennomsiktighet for bobler</translation>
+    </message>
+    <message>
+        <source>Markdown (beta)</source>
+        <translation>Markdown (beta)</translation>
+    </message>
+    <message>
+        <source>Use markdown formated text in messages</source>
+        <translation>Bruk markdown-formatert tekst i meldinger</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Ingen</translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation>Liten</translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation>Medium</translation>
+    </message>
+    <message>
+        <source>Large</source>
+        <translation>Stor</translation>
+    </message>
+    <message>
+        <source>Page padding</source>
+        <translation>Padding for side</translation>
+    </message>
+    <message>
+        <source>Cache size: </source>
+        <translation>Cache-størrelse: </translation>
+    </message>
+    <message>
+        <source>Clear cache</source>
+        <translation>Tøm cache</translation>
+    </message>
+    <message>
+        <source>Send message icon</source>
+        <translation>Ikon for Send melding</translation>
+    </message>
+    <message>
+        <source>Mail Icon</source>
+        <translation>Ikon for e-post</translation>
+    </message>
+    <message>
+        <source>Send Icon</source>
+        <translation>Ikon for send</translation>
+    </message>
+    <message>
+        <source>Reaction size</source>
+        <translation>Størrelse på reaksjon</translation>
+    </message>
+    <message>
+        <source>Channels view</source>
+        <translation>Kanaler</translation>
+    </message>
+    <message>
+        <source>Search field</source>
+        <translation>Søkefelt</translation>
+    </message>
+    <message>
+        <source>Show search field in channels view</source>
+        <translation>Vis søkefelt i kanaloversikten</translation>
+    </message>
+    <message>
+        <source>Messages view</source>
+        <translation>Meldingsvisning</translation>
+    </message>
+    <message>
+        <source>Enable send photo</source>
+        <translation>Tillat å sende bilder</translation>
+    </message>
+    <message>
+        <source>Enable send Photo in attach menu</source>
+        <translation>Tillat «Send bilde» i vedleggsmenyen</translation>
+    </message>
+    <message>
+        <source>System</source>
+        <translation>System</translation>
+    </message>
+    <message>
+        <source>Use logging in to standart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>output</source>
+        <translation>Utdata</translation>
+    </message>
+    <message>
+        <source>Log level (to stderr)</source>
+        <translation>Loggnivå (til stderr)</translation>
+    </message>
+    <message>
+        <source>Use logging</source>
+        <translation>Bruk logging</translation>
+    </message>
+</context>
+<context>
+    <name>ParentMessageItem</name>
+    <message>
+        <source>Reply to</source>
+        <translation>Svar på</translation>
+    </message>
+    <message>
+        <source>Edit message from</source>
+        <translation>Rediger melding fra </translation>
+    </message>
+    <message>
+        <source>Answer to message from</source>
+        <translation>Svar på melding fra</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Login failed because of invalid password</source>
+        <translation>Innloggingen feilet på grunn av feil passord</translation>
+    </message>
+    <message>
+        <source>%0 bytes</source>
+        <translation>%0 bytes</translation>
+    </message>
+    <message>
+        <source>%0 Kb</source>
+        <translation>%0 Kb</translation>
+    </message>
+    <message>
+        <source>%0 Mb</source>
+        <translation>%0 Mb</translation>
+    </message>
+    <message>
+        <source>(you)</source>
+        <translation>(du)</translation>
+    </message>
+    <message>
+        <source>somebody</source>
+        <translation>noen</translation>
+    </message>
+    <message>
+        <source>(edited)</source>
+        <translation>(redigert)</translation>
+    </message>
+    <message>
+        <source>Bytes</source>
+        <translation>Bytes</translation>
+    </message>
+    <message>
+        <source>KB</source>
+        <translation>KB</translation>
+    </message>
+    <message>
+        <source>MB</source>
+        <translation>MB</translation>
+    </message>
+    <message>
+        <source>GB</source>
+        <translation>GB</translation>
+    </message>
+    <message>
+        <source>TB</source>
+        <translation>TB</translation>
+    </message>
+    <message>
+        <source>PB</source>
+        <translation>PB</translation>
+    </message>
+</context>
+<context>
+    <name>ReactionsPage</name>
+    <message>
+        <source>Reactions</source>
+        <translation>Reaksjoner</translation>
+    </message>
+</context>
+<context>
+    <name>TeamOptions</name>
+    <message>
+        <source>Team Options</source>
+        <translation>Teaminnstillinger</translation>
+    </message>
+</context>
+<context>
+    <name>TeamsPage</name>
+    <message>
+        <source>Options</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+</TS>
