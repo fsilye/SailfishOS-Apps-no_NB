632c8f8d415d9124b539c90d133a361ddb3ac7e8
Frank Paul Silye <frankps@gmail.com>
2026-03-30 21:56:24 +0200

    Add files via upload

---
+++ harbour-tarkka-no_NB.ts
@@ -0,0 +1,249 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="no_NB">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Tarkka</source>
+        <translation>Om Tarkka</translation>
+    </message>
+    <message>
+        <source>Key features</source>
+        <translation>Nøkkelfunksjoner</translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation>Kilder</translation>
+    </message>
+    <message>
+        <source>Feedback</source>
+        <translation>Tilbakemeldinger</translation>
+    </message>
+    <message>
+        <source>If you want to provide feedback or report an issue, please use GitHub.</source>
+        <translation>Hvis du ønsker å gi tilbakemeldinger eller rapportere et problem, vennligst bruk GitHub.</translation>
+    </message>
+    <message>
+        <source>Issues</source>
+        <translation>Problemer</translation>
+    </message>
+    <message>
+        <source>Support</source>
+        <translation>Brukerstøtte</translation>
+    </message>
+    <message>
+        <source>If you like my work and want to buy me a beer, feel free to do it!</source>
+        <translation>Hvis du liker arbeidet mitt og vil spandere en øl, er du hjertelig velkommen til det!</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Donere</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>Takk!</translation>
+    </message>
+    <message>
+        <source>Thanks to piggz and his amazing work on &lt;a href=&apos;https://github.com/piggz/harbour-advanced-camera/tree/master&apos;&gt;Advancd Camera&lt;/a&gt;, exposing the filter logic helped me immensely.</source>
+        <translation>Takk til piggz og hans fantastiske arbeid med <a href='https://github.com/piggz/harbour-advanced-camera/tree/master'>Advanced Camera</a>; å få se hvordan filterlogikken var bygget opp hjalp meg mye.</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Oversettelser</translation>
+    </message>
+    <message>
+        <source>Request a new language or contribute to existing languages.</source>
+        <translation>Foreslå et nytt språk eller med eksisterende oversettelser.</translation>
+    </message>
+    <message>
+        <source>Transifex</source>
+        <translation>Transifex</translation>
+    </message>
+    <message>
+        <source>Tarkka is a digital magnifier designed specifically for Sailfish OS. It leverages native camera capabilities to help you observe details clearly.</source>
+        <translation>Tarkka er et digitalt forstørrelsesglass designet spesielt for Sailfish OS. Den utnytter kameraets innebygde funksjoner for å hjelpe deg med å se detaljer tydelig.</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=&apos;https://github.com/fravaccaro/harbour-tarkka/blob/main/LICENSE&apos;&gt;GNU GPLv3&lt;/a&gt; license.</source>
+        <translation>Utgitt under lisensen <a href='https://github.com/fravaccaro/harbour-tarkka/blob/main/LICENSE'>GNU GPLv3</a>.</translation>
+    </message>
+    <message>
+        <source>Smooth digital zoom up to 4x.</source>
+        <translation>Trinnløs digital zoom opptil 4x.</translation>
+    </message>
+    <message>
+        <source>Torch support for low-light environments.</source>
+        <translation>Støtte for lommelykt i omgivelser med lite lys.</translation>
+    </message>
+    <message>
+        <source>Minimalist UI optimized for one-handed use.</source>
+        <translation>MMinimalistisk brukergrensesnitt optimalisert for enhåndsbruk.</translation>
+    </message>
+    <message>
+        <source>Grayscale</source>
+        <translation>Gråskala</translation>
+    </message>
+    <message>
+        <source>Solarize</source>
+        <translation>Solarisering</translation>
+    </message>
+    <message>
+        <source>Whiteboard</source>
+        <translation>Whiteboard</translation>
+    </message>
+    <message>
+        <source>Blackboard</source>
+        <translation>Tavle</translation>
+    </message>
+    <message>
+        <source>Specialized filters:</source>
+        <translation>Spesialfiltre:</translation>
+    </message>
+    <message>
+        <source>Brightness adjustment.</source>
+        <translation>Justering av lysstyrke.</translation>
+    </message>
+    <message>
+        <source>Contrast adjustment.</source>
+        <translation>Kontrastjustering.</translation>
+    </message>
+    <message>
+        <source>Negative</source>
+        <translation>Negativ.</translation>
+    </message>
+    <message>
+        <source>Yellow on black</source>
+        <translation>Gul på svart</translation>
+    </message>
+    <message>
+        <source>Black on yellow</source>
+        <translation>Svart på gul</translation>
+    </message>
+    <message>
+        <source>Yellow on blue</source>
+        <translation>Gul på blå</translation>
+    </message>
+    <message>
+        <source>Blue on yellow</source>
+        <translation>Blå på gul</translation>
+    </message>
+    <message>
+        <source>White on blue</source>
+        <translation>Vit på blå</translation>
+    </message>
+    <message>
+        <source>Blue on white</source>
+        <translation>Blå på vit</translation>
+    </message>
+    <message>
+        <source>Red on black</source>
+        <translation>Röd på svart</translation>
+    </message>
+    <message>
+        <source>Black on red</source>
+        <translation>Svart på röd</translation>
+    </message>
+</context>
+<context>
+    <name>FilterList</name>
+    <message>
+        <source>None</source>
+        <translation>Inget</translation>
+    </message>
+    <message>
+        <source>Grayscale</source>
+        <translation>Gråskala</translation>
+    </message>
+    <message>
+        <source>Negative</source>
+        <translation>Negativ</translation>
+    </message>
+    <message>
+        <source>Solarize</source>
+        <translation>Solarisera</translation>
+    </message>
+    <message>
+        <source>Whiteboard</source>
+        <translation>Vita-tavlan</translation>
+    </message>
+    <message>
+        <source>Blackboard</source>
+        <translation>Svarta-tavlan</translation>
+    </message>
+    <message>
+        <source>Yellow on black</source>
+        <translation>Gul på svart</translation>
+    </message>
+    <message>
+        <source>Black on yellow</source>
+        <translation>Svart på gul</translation>
+    </message>
+    <message>
+        <source>Yellow on blue</source>
+        <translation>Gul på blå</translation>
+    </message>
+    <message>
+        <source>Blue on yellow</source>
+        <translation>Blå på gul</translation>
+    </message>
+    <message>
+        <source>White on blue</source>
+        <translation>Vit på blå</translation>
+    </message>
+    <message>
+        <source>Blue on white</source>
+        <translation>Blå på vit</translation>
+    </message>
+    <message>
+        <source>Red on black</source>
+        <translation>Röd på svart</translation>
+    </message>
+    <message>
+        <source>Black on red</source>
+        <translation>Svart på röd</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Open</source>
+        <translation>Öppna</translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <translation>Dela</translation>
+    </message>
+    <message>
+        <source>Filters</source>
+        <translation>Filter</translation>
+    </message>
+    <message>
+        <source>Brightness</source>
+        <translation>Ljusstyrka</translation>
+    </message>
+    <message>
+        <source>Contrast</source>
+        <translation>Kontrast</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Image saved</source>
+        <translation>Bild sparad</translation>
+    </message>
+    <message>
+        <source>Image saved in the gallery as </source>
+        <translation>Bilden sparad i Galleri som </translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fel</translation>
+    </message>
+    <message>
+        <source>Error saving the image in the gallery</source>
+        <translation>Ett fel uppstod när bilden skulle sparas i Galleri</translation>
+    </message>
+</context>
+</TS>
