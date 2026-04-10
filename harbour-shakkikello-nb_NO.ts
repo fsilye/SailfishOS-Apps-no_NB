612e4ed1470921ea676f3a28b4278ff9e39f2ca6
Frank Paul Silye <frankps@gmail.com>
2026-04-10 05:29:36 +0200

    Add files via upload

---
+++ harbour-shakkikello-nb_NO.ts
@@ -0,0 +1,564 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>About</name>
+    <message>
+        <source>Back to settings</source>
+        <translation>Tilbake til innstillinger</translation>
+    </message>
+    <message>
+        <source>About page</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Fast chess, version</source>
+        <extracomment>The name of the app followed with a version number</extracomment>
+        <translation>Fast chess, versjon</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Oversettelser</translation>
+    </message>
+    <message>
+        <source>German (Nit)</source>
+        <translation>Tysk (Nit)</translation>
+    </message>
+    <message>
+        <source>Spanish (Carlos Gonzalez)</source>
+        <translation>Spansk (Carlos Gonzalez)</translation>
+    </message>
+    <message>
+        <source>Finnish (Riku Lahtinen)</source>
+        <translation>Finsk (Riku Lahtinen)</translation>
+    </message>
+    <message>
+        <source>French (lutinotmalin)</source>
+        <translation>Fransk (lutinotmalin)</translation>
+    </message>
+    <message>
+        <source>Hungarian (leoka)</source>
+        <translation>Ungarsk (leoka)</translation>
+    </message>
+    <message>
+        <source>Dutch (Heimen Stoffels)</source>
+        <translation>Nederlandsk (Heimen Stoffels)</translation>
+    </message>
+    <message>
+        <source>Polish (szopin)</source>
+        <translation>Polsk (szopin)</translation>
+    </message>
+    <message>
+        <source>Chinese (China) (Historyscholar)</source>
+        <translation>Kinesisk (Kina) (Historyscholar)</translation>
+    </message>
+    <message>
+        <source>Contributions</source>
+        <translation>Bidrag</translation>
+    </message>
+    <message>
+        <source>The design of the pieces (Kapu)</source>
+        <translation>Brikkedesign (Kapu)</translation>
+    </message>
+    <message>
+        <source>Third party software</source>
+        <translation>Tredjepartsprogramvare</translation>
+    </message>
+    <message>
+        <source>Licence</source>
+        <translation>Lisens</translation>
+    </message>
+    <message>
+        <source>Copyright (c) 2015, Riku Lahtinen</source>
+        <translation>Opphavsrett (c) 2015, Riku Lahtinen</translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3. License, source code and more information:</source>
+        <translation>Lisensiert under GPLv3. Lisens, kildekode og mer informasjon:</translation>
+    </message>
+    <message>
+        <source>Dutch (Belgium) (Nathan Follens)</source>
+        <translation>Nederlandsk (Belgia) (Nathan Follens)</translation>
+    </message>
+    <message>
+        <source>Stockfish engine, v6 (stockfishchess.org)</source>
+        <translation>Stockfish-motor, v6 (stockfishchess.org)</translation>
+    </message>
+</context>
+<context>
+    <name>Boardview</name>
+    <message>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <source>Save and manage games</source>
+        <translation>Lagre og administrer spill</translation>
+    </message>
+    <message>
+        <source>Show moves</source>
+        <translation>Vis trekk</translation>
+    </message>
+    <message>
+        <source>Check!</source>
+        <translation>Sjakk!</translation>
+    </message>
+    <message>
+        <source>Checkmate!</source>
+        <translation>Sjakk matt!</translation>
+    </message>
+    <message>
+        <source>Stalemate!</source>
+        <translation>Patt!</translation>
+    </message>
+    <message>
+        <source>White won!</source>
+        <translation>Hvit vant!</translation>
+    </message>
+    <message>
+        <source>Black won!</source>
+        <translation>Svart vant!</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Fortsett</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Nullstill</translation>
+    </message>
+    <message>
+        <source>Chess board</source>
+        <translation>Sjakkbrett</translation>
+    </message>
+    <message>
+        <source>min:s</source>
+        <translation>min:sek</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>New game</source>
+        <translation>Nytt spill</translation>
+    </message>
+</context>
+<context>
+    <name>Clockview</name>
+    <message>
+        <source>Black´s turn</source>
+        <translation>Svart sin tur</translation>
+    </message>
+    <message>
+        <source>White´s turn</source>
+        <translation>Hvit sin tur</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Fortsett</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Nullstill</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <source>Chess clock</source>
+        <translation>Sjakklokke</translation>
+    </message>
+    <message>
+        <source>min:s</source>
+        <extracomment>Minutes:seconds</extracomment>
+        <translation>min:sek</translation>
+    </message>
+    <message>
+        <source>White´s clock</source>
+        <translation>Hvits klokke</translation>
+    </message>
+    <message>
+        <source>Black´s clock</source>
+        <translation>Svarts klokke</translation>
+    </message>
+</context>
+<context>
+    <name>Connbox</name>
+    <message>
+        <source>TCP connection</source>
+        <translation>TCP-tilkobling</translation>
+    </message>
+    <message>
+        <source>My device info</source>
+        <translation>Min enhetsinfo</translation>
+    </message>
+    <message>
+        <source>Opponent&apos;s device info</source>
+        <translation>Motstanders enhetsinfo</translation>
+    </message>
+    <message>
+        <source>Test connection</source>
+        <translation>Test tilkobling</translation>
+    </message>
+    <message>
+        <source>Color mismatch, change my color</source>
+        <extracomment>Reports the player that colors selected in games in different devices prevent the game start. Another player has to change the color the game to proceed.</extracomment>
+        <translation>Fargekonflikt, bytt min farge</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Nullstill</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Test in progress</source>
+        <translation>Tester...</translation>
+    </message>
+    <message>
+        <source>Waiting your opponent to start</source>
+        <translation>Venter på at din motstander starter</translation>
+    </message>
+    <message>
+        <source>Retest the connection</source>
+        <translation>Test tilkobling på nytt</translation>
+    </message>
+    <message>
+        <source>IP address</source>
+        <translation>IP-adresse</translation>
+    </message>
+    <message>
+        <source>Port number</source>
+        <translation>Portnummer</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Fast chess</source>
+        <translation>Fast chess</translation>
+    </message>
+</context>
+<context>
+    <name>GameInfo</name>
+    <message>
+        <source>Tag pairs</source>
+        <extracomment>&quot;Tag pairs&quot; could be also e.g. &quot;Game info&quot;. This section of the page has the general information of the game. See more https://en.wikipedia.org/wiki/Portable_Game_Notation</extracomment>
+        <translation>Info-tagger</translation>
+    </message>
+    <message>
+        <source>Movetext, Coordinate notation</source>
+        <extracomment>&quot;Movetext, Coordinate notation&quot; could be also e.g. &quot;Moves in coordinates&quot;. See more https://en.wikipedia.org/wiki/Portable_Game_Notation</extracomment>
+        <translation>Trekkliste, koordinatnotasjon</translation>
+    </message>
+    <message>
+        <source>Game</source>
+        <translation>Spill</translation>
+    </message>
+</context>
+<context>
+    <name>GameInfo2</name>
+    <message>
+        <source>Current game</source>
+        <translation>Gjeldende spill</translation>
+    </message>
+    <message>
+        <source>Tag pairs</source>
+        <extracomment>&quot;Tag pairs&quot; could be also &quot;Game info&quot;. This section of the page has the general information of the game. See more https://en.wikipedia.org/wiki/Portable_Game_Notation</extracomment>
+        <translation>Info-tagger</translation>
+    </message>
+    <message>
+        <source>Movetext, Coordinate notation</source>
+        <extracomment>&quot;Movetext, Coordinate notation&quot; could be also e.g. &quot;Moves in coordinates&quot;. See more https://en.wikipedia.org/wiki/Portable_Game_Notation</extracomment>
+        <translation>Trekkliste, koordinatnotasjon</translation>
+    </message>
+</context>
+<context>
+    <name>GameList</name>
+    <message>
+        <source>Save current game</source>
+        <translation>Lagre gjeldende spill</translation>
+    </message>
+    <message>
+        <source>Game list</source>
+        <translation>Spilliste</translation>
+    </message>
+    <message>
+        <source>Game</source>
+        <translation>Spill</translation>
+    </message>
+    <message>
+        <source>Show game</source>
+        <translation>Vis spill</translation>
+    </message>
+    <message>
+        <source>Delete game</source>
+        <translation>Slett spill</translation>
+    </message>
+    <message>
+        <source>Animate game</source>
+        <translation>Animer spill</translation>
+    </message>
+</context>
+<context>
+    <name>GameSelector</name>
+    <message>
+        <source>Game</source>
+        <translation>Spill</translation>
+    </message>
+    <message>
+        <source>Select game</source>
+        <translation>Velg spill</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Save settings</source>
+        <translation>Lagre innstillinger</translation>
+    </message>
+    <message>
+        <source>Play chess</source>
+        <translation>Spill sjakk</translation>
+    </message>
+    <message>
+        <source>Clock view</source>
+        <translation>Klokkevisning</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>-1 min</source>
+        <extracomment>Reduce time by 1 minute</extracomment>
+        <translation>-1 min</translation>
+    </message>
+    <message>
+        <source>+1 min</source>
+        <extracomment>Increase time by 1 minute</extracomment>
+        <translation>+1 min</translation>
+    </message>
+    <message>
+        <source>-1 s</source>
+        <extracomment>Reduce time by 1 second</extracomment>
+        <translation>-1 sek</translation>
+    </message>
+    <message>
+        <source>+1 s</source>
+        <extracomment>Increase time by 1 second</extracomment>
+        <translation>+1 sek</translation>
+    </message>
+    <message>
+        <source>min</source>
+        <extracomment>min is an abbreviation of a minute</extracomment>
+        <translation>min</translation>
+    </message>
+    <message>
+        <source>s</source>
+        <extracomment>s is an abbreviation of a second</extracomment>
+        <translation>sek</translation>
+    </message>
+    <message>
+        <source>Settings page</source>
+        <translation>Innstillingsside</translation>
+    </message>
+    <message>
+        <source>Clock settings</source>
+        <translation>Innstillinger for klokke</translation>
+    </message>
+    <message>
+        <source>White</source>
+        <translation>Hvit</translation>
+    </message>
+    <message>
+        <source>Black</source>
+        <translation>Svart</translation>
+    </message>
+    <message>
+        <source>Increment/move</source>
+        <extracomment>Sets how many seconds are added to the total remaining time of the game per move.</extracomment>
+        <translation>Tilleggstid per trekk</translation>
+    </message>
+    <message>
+        <source>Time counting</source>
+        <extracomment>The time is counted upwards from zero to max or downwards from max to zero. This is label for that ComboBox.</extracomment>
+        <translation>Tidsmåling</translation>
+    </message>
+    <message>
+        <source>Downwards</source>
+        <translation>Nedtelling</translation>
+    </message>
+    <message>
+        <source>Upwards</source>
+        <translation>Oppover</translation>
+    </message>
+    <message>
+        <source>Chess settings</source>
+        <translation>Sjakkinnstillinger</translation>
+    </message>
+    <message>
+        <source>Opponent</source>
+        <translation>Motstander</translation>
+    </message>
+    <message>
+        <source>Stockfish</source>
+        <extracomment>Stockfish is a name of the chess engine, more info https://stockfishchess.org/</extracomment>
+        <translation>Stockfish</translation>
+    </message>
+    <message>
+        <source>Human</source>
+        <translation>Menneske</translation>
+    </message>
+    <message>
+        <source>Another device</source>
+        <translation>Annen enhet</translation>
+    </message>
+    <message>
+        <source>Port number</source>
+        <translation>Portnummer</translation>
+    </message>
+    <message>
+        <source>Random</source>
+        <translation>Tilfeldig</translation>
+    </message>
+    <message>
+        <source>Fixed</source>
+        <translation>Fast</translation>
+    </message>
+    <message>
+        <source>My color</source>
+        <translation>Min farge</translation>
+    </message>
+    <message>
+        <source>Opening</source>
+        <translation>Åpning</translation>
+    </message>
+    <message>
+        <source>Saved game</source>
+        <translation>Lagret spill</translation>
+    </message>
+    <message>
+        <source>Skill Level</source>
+        <translation>Ferdighetsnivå</translation>
+    </message>
+    <message>
+        <source>Movetime</source>
+        <extracomment>Sets the time the Stockfish engine has per move.</extracomment>
+        <translation>Tid per trekk</translation>
+    </message>
+    <message>
+        <source>View settings</source>
+        <translation>Visningsinnstillinger</translation>
+    </message>
+    <message>
+        <source>Default view</source>
+        <translation>Standardvisning</translation>
+    </message>
+    <message>
+        <source>Chess clock</source>
+        <translation>Sjakklokke</translation>
+    </message>
+    <message>
+        <source>Chess board</source>
+        <translation>Sjakkbrett</translation>
+    </message>
+    <message>
+        <source>Unlike</source>
+        <extracomment>The style of the pieces is unlike</extracomment>
+        <translation>Ulike</translation>
+    </message>
+    <message>
+        <source>Classic</source>
+        <extracomment>The style of the pieces is classic</extracomment>
+        <translation>Klassisk</translation>
+    </message>
+    <message>
+        <source>Style of the pieces</source>
+        <extracomment>The style of the pieces selector</extracomment>
+        <translation>Stil på brikkene</translation>
+    </message>
+    <message>
+        <source>Personal art</source>
+        <extracomment>Player can select the pieces of her or his choice</extracomment>
+        <translation>Egendefinert grafikk</translation>
+    </message>
+</context>
+<context>
+    <name>Settings_dialog_personal_art</name>
+    <message>
+        <source>Files required</source>
+        <translation>Nødvendige filer</translation>
+    </message>
+    <message>
+        <source>Testing</source>
+        <translation>Testing</translation>
+    </message>
+    <message>
+        <source>Current folder path</source>
+        <translation>Gjeldende mappebane</translation>
+    </message>
+    <message>
+        <source>If the current folder path is already right, please cancel this operation.</source>
+        <translation>Hvis gjeldende mappebane allerede er riktig, vennligst avbryt denne operasjonen.</translation>
+    </message>
+    <message>
+        <source>File selection</source>
+        <translation>Filvalg</translation>
+    </message>
+    <message>
+        <source>Your personal art folder should have all the image files as follows:</source>
+        <translation>Mappen med egendefinert grafikk må inneholde følgende bildefiler:</translation>
+    </message>
+    <message>
+        <source>For the start of the testing of this feature you can copy the required files from the device path:</source>
+        <translation>For å teste denne funksjonen kan du kopiere de nødvendige filene fra banen:</translation>
+    </message>
+    <message>
+        <source>Selecting only one image file from your personal art folder is required.</source>
+        <translation>Du må velge én bildefil fra mappen med egendefinert grafikk.</translation>
+    </message>
+</context>
+<context>
+    <name>funktiot</name>
+    <message>
+        <source>Reset</source>
+        <translation>Nullstill</translation>
+    </message>
+    <message>
+        <source>New game</source>
+        <translation>Nytt spill</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-shakkikello</name>
+    <message>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <source>Downwards</source>
+        <translation>Nedtelling</translation>
+    </message>
+</context>
+</TS>
